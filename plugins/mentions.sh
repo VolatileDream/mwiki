f5297c9f2a8675e410240b1c7ff33ea7b0613088
Gianni Gambetti <gianni@gambetti.ca>
2015-05-22 00:25:45 -0400

    Adding mentions/backlinks plugin
    Since mwiki uses a build tool to generate it's files, the way that mentions/
     backlinks must be specified is with the creation of a command pipeline. In Tup
     it looks like this (assume !m_build and !mentions):
    : foreach *.mw |> !m_build ; !mentions |> build/%B.html
    
    However, Tup doesn't handle this macro expansion well, so instead we must
     expand the macros by hand, writing this instead:
    : foreach *.mw | *.mw |> mwiki generate %B %o ; plugins/mentions.sh %B %o |> build/%B.html

---
+++ plugins/mentions.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# mwiki expects plugins to be added to the .mwiki.d/Tupfile, which will ensure
+# that the get run and are kept up to date when files are changed.
+
+# Here is the mentions entry:
+
+## !mentions = | *.mw |> plugins/mentions.sh %B %o |>
+
+# Note that it must get added to the pipeline of regular page generation.
+
+# mwiki (through TUP) runs plugins inside it's mwiki directory.
+MWIKI_DIR="."
+
+page_mentions(){
+	local page="$1" ; shift
+	grep --files-with-match --ignore-case "[#@]$page" "$MWIKI_DIR/"*.mw |
+	 sed "s_^$MWIKI_DIR/__" | sed 's_.mw$__'
+	 sort -u
+}
+
+run(){
+	local page="$1" ; shift
+	local output="$1" ; shift
+	( echo "<h2>Mentions</h2><hr>" ;
+	  page_mentions "$page" |
+	   sed 's_^.*$_<a href="./&.html">&</a>_' ) |
+	awk '{ print $0 " <br>" }' >> "$output"
+}
+
+run "$@"
