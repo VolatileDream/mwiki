b808030ff3895b284a7f6f3bd66e3c647a32131a
Gianni Gambetti <gianni@gambetti.ca>
2015-05-22 00:32:42 -0400

    updating mentions.sh docs

--- plugins/mentions.sh
+++ plugins/mentions.sh
@@ -8,6 +8,8 @@
 ## !mentions = | *.mw |> plugins/mentions.sh %B %o |>
 
 # Note that it must get added to the pipeline of regular page generation.
+# Because of the way Tup treats macro expansion, the macros that would generate
+# the build/%B.html file must all be expanded by hand.
 
 # mwiki (through TUP) runs plugins inside it's mwiki directory.
 MWIKI_DIR="."
