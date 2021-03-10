60c1e3aa942bf76f92630f6784a0e3bfdb155d8b
Gianni Gambetti <gianni@gambetti.ca>
2021-03-10 03:28:45 +0100

    Adds Event Plugin to default config

--- py-install/config.py
+++ py-install/config.py
@@ -1,10 +1,12 @@
 
+from events import EventsPlugin
 from html import HtmlRender
 from index import IndexRender
 from mentions import MentionsPlugin
 from missing import MissingPlugin
 from quotes import QuotesPlugin
 
+register(EventsPlugin())
 register(HtmlRender())
 register(IndexRender())
 register(MissingPlugin())
