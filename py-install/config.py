1ee0319b05f07e3736f33e9a9c159b5b73ea7b6e
Gianni Gambetti <gianni@gambetti.ca>
2021-03-10 01:39:23 +0100

    Add new plugins to template
    Adds Mentions and Missing to the default template.

--- py-install/config.py
+++ py-install/config.py
@@ -1,8 +1,12 @@
 
 from html import HtmlRender
 from index import IndexRender
+from mentions import MentionsPlugin
+from missing import MissingPlugin
 from quotes import QuotesPlugin
 
 register(HtmlRender())
 register(IndexRender())
+register(MissingPlugin())
+register(MentionsPlugin())
 register(QuotesPlugin())
